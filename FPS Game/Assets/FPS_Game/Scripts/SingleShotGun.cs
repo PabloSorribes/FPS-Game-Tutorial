53c70f8a9afcafb6c90726a52f5d79fcc3b13276
PabloSorribes <pablo.sorribes.bernhard@gmail.com>
2021-02-23 16:58:46 +0100

    Separate Events for Local vs Server works!

--- FPS Game/Assets/FPS_Game/Scripts/SingleShotGun.cs
+++ FPS Game/Assets/FPS_Game/Scripts/SingleShotGun.cs
@@ -16,31 +16,12 @@ public class SingleShotGun : Gun
 
 	PhotonView PV;
 
-
-    bool isServer = false;
 	void Awake()
 	{
 		PV = GetComponent<PhotonView>();
 
 		snapshotEmitter.Play();
 	}
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            isServer = true;
-            Debug.Log("tremolo 1.");
-           // snapshotEmitter.SetParameter("TremoloParam", 1f);
-
-        }
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            isServer = false;
-            Debug.Log("tremolo 0");
-           // snapshotEmitter.SetParameter("TremoloParam", 0f);
-        }
-    }
 
     public override void Use()
 	{
@@ -50,41 +31,26 @@ public class SingleShotGun : Gun
 	void Shoot()
 	{
 		// Play Shoot Sound for local player
-        FMODUnity.RuntimeManager.PlayOneShot(localEvent, transform.position);
-        snapshotEmitter.SetParameter("TremoloParam", 0f);
+		FMODUnity.RuntimeManager.PlayOneShot(localEvent, transform.position);
+		snapshotEmitter.SetParameter("TremoloParam", 0f);
 
 		// Send out event to play the local shoot sound on everyone else's computer.
-		PV.RPC(nameof(RPC_ServerShootAudio), RpcTarget.All);
+		PV.RPC(nameof(RPC_ServerShootAudio), RpcTarget.Others);
 
-		// snapshotEmitter.SetParameter("TremoloParam", 0f);
-
-		//      PV.RPC(nameof(RPC_ServerShootAudio), RpcTarget.All);
-
-		//Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
-		//ray.origin = cam.transform.position;
-		//if (Physics.Raycast(ray, out RaycastHit hit))
-		//{
-		//	hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo)itemInfo).damage);
-		//	PV.RPC("RPC_Shoot", RpcTarget.All, hit.point, hit.normal);
-		//}
+		Debug.Log($"LocalPlayer: {PhotonNetwork.LocalPlayer.ActorNumber}");
 	}
 
     [PunRPC]
 	private void RPC_ServerShootAudio()
 	{
 		FMODUnity.RuntimeManager.PlayOneShot(serverEvent, transform.position);
-        snapshotEmitter.SetParameter("TremoloParam", 1f);
-    }
+		snapshotEmitter.SetParameter("TremoloParam", 1f);
+		Debug.Log($"ServerPlayer: {PhotonNetwork.LocalPlayer.ActorNumber}");
 
-    [PunRPC]
-	void RPC_Shoot(Vector3 hitPosition, Vector3 hitNormal)
-	{
-		Collider[] colliders = Physics.OverlapSphere(hitPosition, 0.3f);
-		if (colliders.Length != 0)
+
+		if (!PhotonNetwork.LocalPlayer.IsLocal)
 		{
-			GameObject bulletImpactObj = Instantiate(bulletImpactPrefab, hitPosition + hitNormal * 0.001f, Quaternion.LookRotation(hitNormal, Vector3.up) * bulletImpactPrefab.transform.rotation);
-			Destroy(bulletImpactObj, 10f);
-			bulletImpactObj.transform.SetParent(colliders[0].transform);
+			
 		}
 	}
 }
