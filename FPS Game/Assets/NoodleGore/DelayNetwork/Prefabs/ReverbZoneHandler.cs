6588534c85204f4abb0bf7c7e4426a698f38108e
Anders Hofsten <anders.jakobsen.hofsten@gmail.com>
2021-02-25 12:07:24 +0100

    reverbzone prefab plus partial script.

--- FPS Game/Assets/NoodleGore/DelayNetwork/Prefabs/ReverbZoneHandler.cs
+++ FPS Game/Assets/NoodleGore/DelayNetwork/Prefabs/ReverbZoneHandler.cs
@@ -1,13 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sonigon;
 
 public class ReverbZoneHandler : MonoBehaviour
 {
+
+    public 
+    public enum ReverbZone
+    {
+        Default,
+        Cave,
+        Ship,
+        Canyon,
+        Mountains
+    }
+    ReverbZone MyReverb;
     private void OnTriggerEnter(Collider other)
     {
+        
+
         // do stuff here on entering the trigger.
+        switch (MyReverb)
+        {
+            case ReverbZone.Default:
+              //  gunshotHandler.convolutionSwitcherEvent.SetParameter("ReverbZone", 0);
+                break;
+            case ReverbZone.Cave:
+              //  gunshotHandler.convolutionSwitcherEvent.SetParameter("ReverbZone", 1);
+                break;
+
+            case ReverbZone.Ship:
+              //  gunshotHandler.convolutionSwitcherEvent.SetParameter("ReverbZone", 2);
+                break;
+
+            case ReverbZone.Canyon:
+              //  gunshotHandler.convolutionSwitcherEvent.SetParameter("ReverbZone", 3);
+                break;
+
+            case ReverbZone.Mountains:
+              //  gunshotHandler.convolutionSwitcherEvent.SetParameter("ReverbZone", 4);
+                break;
+            default:
+                break;
+        }
     }
+
 
     private void OnTriggerStay(Collider other)
     {
