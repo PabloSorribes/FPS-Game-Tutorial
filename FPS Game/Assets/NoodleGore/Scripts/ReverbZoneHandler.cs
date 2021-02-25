6876a658c3f1a4f9425f85437441206c6539f8da
Anders Hofsten <anders.jakobsen.hofsten@gmail.com>
2021-02-25 11:36:29 +0100

    Added reverbzone trigger prefab + script.

---
+++ FPS Game/Assets/NoodleGore/Scripts/ReverbZoneHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverbZoneHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // do stuff here on entering the trigger.
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // do stuff here when inside the trigger (runs in update).
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // do stuff here when exiting trigger.
+    }
+}
