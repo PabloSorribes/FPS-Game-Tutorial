3be25d6fe5984badbd300fae566dfede8da978a9
PabloSorribes <pablo.sorribes.bernhard@gmail.com>
2021-03-01 15:18:53 +0100

    adding a SoundReflectionType-enum to the SoundReflectionData event

--- FPS Game/Assets/10 Sonigon/IndoorOutdoorReverbAmbSystem/Scripts/ScriptableObjects/SoundReflectionData.cs
+++ FPS Game/Assets/10 Sonigon/IndoorOutdoorReverbAmbSystem/Scripts/ScriptableObjects/SoundReflectionData.cs
@@ -7,10 +7,25 @@ namespace Sonigon
 	[CreateAssetMenu(fileName = "SoundReflection_", menuName = "Sonigon/SoundReflection", order = 5)]
 	public class SoundReflectionData : ScriptableObject
 	{
-		[FMODUnity.EventRef]
-		[SerializeField]
-		private string reflectionEvent = null;
-		public string ReflectionEvent => reflectionEvent;
+		//[FMODUnity.EventRef]
+		//[SerializeField]
+		//private string reflectionEvent = null;
+		//public string ReflectionEvent => reflectionEvent;
 
+		public enum SoundReflectionType
+		{
+			Indoor_Dull_Big_Shot = 0,
+			Indoor_Dull_Medium_Shot = 1,
+			Indoor_Dull_Small_Shot = 2,
+			Indoor_Reverbant_Big_Shot = 3,
+			Indoor_Reverbant_Huge_Shot = 4,
+			Indoor_Reverbant_Medium_Shot = 5,
+			Indoor_Reverbant_Small_Shot = 6,
+			Outdoor_Outdoor_Field_Shot = 7,
+			Outdoor_Outdoor_Forest_Shot = 8,
+			Outdoor_Outdoor_Urban_Shot = 9,
+		}
+
+		public SoundReflectionType soundReflectionType;
 	}
 }
