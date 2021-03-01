fa14fc3cbd95745576efd71ad1ffa2d895c16a0c
PabloSorribes <pablo.sorribes.bernhard@gmail.com>
2021-03-01 16:54:10 +0100

    Editor shortcut to start the game from the meneu scene

---
+++ FPS Game/Assets/Editor/EditorShortcuts.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public static class EditorShortcuts
+{
+	[MenuItem("NoodleGore/StartGame %#.", priority = -1000)]
+	public static void StartGame()
+	{
+		if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+		{
+			EditorSceneManager.OpenScene(EditorBuildSettings.scenes[0].path);
+			EditorApplication.isPlaying = true;
+		}
+	}
+}
